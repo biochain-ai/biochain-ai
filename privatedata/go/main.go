589ccd4cf1e41f62e4bca162346245fd9ceb9aa1
Alessio Diana <alessio.diana@studenti.unipr.it>
2022-12-23 11:47:04 +0100

    Added insertData to chaincode privatedata.

--- privatedata/go/main.go
+++ privatedata/go/main.go
@@ -26,8 +26,8 @@ type Chaincode struct {
 
 // Asset used to track ownership of the data, publicly
 type dataProperty struct {
-	Id           string
-	Proprietario string
+	Id    string
+	Owner string
 }
 
 // Asset used to describe the data
@@ -37,9 +37,9 @@ type dataDescription struct {
 }
 
 // Asset used in the private databases to store the actual data
-type datoBioPrivateDetails struct {
+type dataBioPrivateDetails struct {
 	Id   string
-	Dato string
+	Data string
 }
 
 // Enumeration used in assetRequest
@@ -64,6 +64,7 @@ type assetRequest struct {
 // Initializes chaincode
 // =====================
 func (c *Chaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("Init running.")
 	return shim.Success(nil)
 }
 
@@ -71,134 +72,169 @@ func (c *Chaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 // ======
 func (c *Chaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, args := stub.GetFunctionAndParameters()
-	fmt.Println("invoke is running " + function)
+	fmt.Println("Invoke is running " + function)
 
 	switch function {
-	case "inserisciDato":
-		return c.inserisciDato(stub, args)
+	case "insertData":
+		return c.insertData(stub, args)
+	case "removeData":
+		return c.removeData(stub, args)
+	case "viewAllData":
+		return c.viewAllData(stub, args)
+	case "viewPersonalData":
+		return c.viewPersonalData(stub, args)
+	case "requestData":
+		return c.requestData(stub, args)
+	case "viewSharingRequests":
+		return c.viewSharingRequests(stub, args)
+	case "acceptRequest":
+		return c.acceptRequest(stub, args)
+	case "denyRequest":
+		return c.denyRequest(stub, args)
 	default:
-		fmt.Println("Funzione non esistente!")
+		fmt.Println("Unknown function!")
 		return shim.Error("Received unknown function")
 	}
 }
 
 // insertData
 // ==========
-func (c *Chaincode) insertData(stub shim.ChaincodeStubInterface) pb.Response
-
-// removeData
-// ==========
-func (c *Chaincode) removeData(stub shim.ChaincodeStubInterface) pb.Response
-
-// viewAllData
-// ========
-func (c *Chaincode) viewAllData(stub shim.ChaincodeStubInterface) pb.Response
-
-// viewPersonalData
-// ========
-func (c *Chaincode) viewPersonalData(stub shim.ChaincodeStubInterface) pb.Response
-
-// requestData
-// ===========
-func (c *Chaincode) requestData(stub shim.ChaincodeStubInterface) pb.Response
-
-// viewSharingRequests
-// ===================
-func (c *Chaincode) viewSharingRequests(stub shim.ChaincodeStubInterface) pb.Response
-
-// acceptRequest
-// =============
-func (c *Chaincode) acceptRequest(stub shim.ChaincodeStubInterface) pb.Response
-
-// denyRequest
-// ===========
-func (c *Chaincode) denyRequest(stub shim.ChaincodeStubInterface) pb.Response
-
-func (c *Chaincode) inserisciDato(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var err error
-
-	type datoTransientInput struct {
-		Nome         string
-		Proprietario string
-		Dato         string
+func (c *Chaincode) insertData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	type dataTransientInput struct {
+		Name        string
+		Owner       string
+		Description string
+		Data        string
 	}
 
+	var err error
+	var dataInput dataTransientInput
+
+	// No parameter shall be passed outside the transient map
 	if len(args) != 0 {
 		return shim.Error("Incorrect number of parameters. Data must be passed in transient map.")
 	}
 
+	// Retrieve the transient data
 	transMap, err := stub.GetTransient()
 	if err != nil {
 		return shim.Error("Error getting transient: " + err.Error())
 	}
 
-	marbleJsonBytes, ok := transMap["dato"]
+	// Get the 'data' field
+	dataJSONBytes, ok := transMap["data"]
 	if !ok {
-		return shim.Error("dato must be a key in the transient map")
+		return shim.Error("'data' must be a key in the transient map.")
 	}
 
-	if len(marbleJsonBytes) == 0 {
-		return shim.Error("dato value in the transient map must be a non-empty JSON string")
-	}
-
-	var datoInput datoTransientInput
-	err = json.Unmarshal(marbleJsonBytes, &datoInput)
+	// Decode the JSON bytes and store it in a variable
+	err = json.Unmarshal(dataJSONBytes, &dataInput)
 	if err != nil {
-		return shim.Error("Failed to decode JSON of: " + string(marbleJsonBytes))
+		return shim.Error("Error decoding JSON data: " + string(dataJSONBytes))
 	}
 
-	if len(datoInput.Nome) == 0 {
-		return shim.Error("Nome field must be a non-empty string")
+	// Check input consistency
+	if len(dataInput.Data) == 0 {
+		return shim.Error("'Data' field must be a non-empty string.")
 	}
-	if len(datoInput.Proprietario) == 0 {
-		return shim.Error("proprietario field must be a non-empty string")
+	if len(dataInput.Name) == 0 {
+		return shim.Error("'Name' field must be a non-empty string.")
 	}
-	if len(datoInput.Dato) == 0 {
-		return shim.Error("dato field must be a non-empty string")
+	if len(dataInput.Description) == 0 {
+		return shim.Error("'Description' field must be a non-empty string.")
+	}
+	if len(dataInput.Owner) == 0 {
+		return shim.Error("'Owner' field must be a non-empty string.")
 	}
 
-	// ==== Check if marble already exists ====
-	datoAsBytes, err := stub.GetPrivateData("collectionDatoBio", datoInput.Nome)
+	// Check if data already exists
+	dataLedgerBytes, err := stub.GetState(dataInput.Name)
 	if err != nil {
-		return shim.Error("Failed to get dato: " + err.Error())
-	} else if datoAsBytes != nil {
-		fmt.Println("This dato already exists: " + datoInput.Nome)
-		return shim.Error("This dato already exists: " + datoInput.Nome)
+		return shim.Error("Failed to get data from the ledger: " + err.Error())
+	} else if dataLedgerBytes != nil {
+		fmt.Println("This data already exists: " + dataInput.Name)
+		return shim.Error("This data already exists: " + dataInput.Name)
 	}
 
-	// ==== Create marble object and marshal to JSON ====
-	dato := &datoBio{
-		Nome:         datoInput.Nome,
-		Proprietario: datoInput.Proprietario,
-	}
-	datoJSONasBytes, err := json.Marshal(dato)
-	if err != nil {
-		return shim.Error(err.Error())
+	// Create data to be inserted into the ledger
+	dataDesc := &dataDescription{
+		Id:          dataInput.Name,
+		Description: dataInput.Description,
 	}
 
-	// === Save marble to state ===
-	err = stub.PutPrivateData("collectionDatoBio", datoInput.Nome, datoJSONasBytes)
-	if err != nil {
-		return shim.Error(err.Error())
+	dataProp := &dataProperty{
+		Id:    dataInput.Name,
+		Owner: dataInput.Owner,
 	}
 
-	// ==== Create marble private details object with price, marshal to JSON, and save to state ====
-	datoBioPrivateDetails := &datoBioPrivateDetails{
-		Nome: datoInput.Nome,
-		Dato: datoInput.Dato,
-	}
-	datoBioPrivateDetailsBytes, err := json.Marshal(datoBioPrivateDetails)
+	dataDescJSON, err := json.Marshal(dataDesc)
 	if err != nil {
-		return shim.Error(err.Error())
-	}
-	err = stub.PutPrivateData("collectionDatoBioPrivateDetails", datoInput.Nome, datoBioPrivateDetailsBytes)
-	if err != nil {
-		return shim.Error(err.Error())
+		return shim.Error("Error creating data:" + err.Error())
 	}
 
-	fmt.Println("- end init marble")
+	dataPropJSON, err := json.Marshal(dataProp)
+	if err != nil {
+		return shim.Error("Error creating data:" + err.Error())
+	}
+
+	// Save data into the ledger
+	err = stub.PutState(dataInput.Name, dataDescJSON)
+	if err != nil {
+		return shim.Error("Error during put state: " + err.Error())
+	}
+
+	err = stub.PutState(dataInput.Name, dataPropJSON)
+	if err != nil {
+		return shim.Error("Error during put state: " + err.Error())
+	}
+
+	// Create data to be insterted into the private data
+	dataBio := &dataBioPrivateDetails{
+		Id:   dataInput.Name,
+		Data: dataInput.Data,
+	}
+
+	dataBioJSON, err := json.Marshal(dataBio)
+	if err != nil {
+		return shim.Error("Error creating data: " + err.Error())
+	}
+
+	err = stub.PutPrivateData("collectionDatoBioPrivateDetails", dataInput.Name, dataBioJSON)
+	if err != nil {
+		return shim.Error("Error during put private state: " + err.Error())
+	}
+
+	fmt.Println("end insertData")
 	return shim.Success(nil)
 }
+
+// removeData
+// ==========
+func (c *Chaincode) removeData(stub shim.ChaincodeStubInterface, args []string) pb.Response
+
+// viewAllData
+// ========
+func (c *Chaincode) viewAllData(stub shim.ChaincodeStubInterface, args []string) pb.Response
+
+// viewPersonalData
+// ========
+func (c *Chaincode) viewPersonalData(stub shim.ChaincodeStubInterface, args []string) pb.Response
+
+// requestData
+// ===========
+func (c *Chaincode) requestData(stub shim.ChaincodeStubInterface, args []string) pb.Response
+
+// viewSharingRequests
+// ===================
+func (c *Chaincode) viewSharingRequests(stub shim.ChaincodeStubInterface, args []string) pb.Response
+
+// acceptRequest
+// =============
+func (c *Chaincode) acceptRequest(stub shim.ChaincodeStubInterface, args []string) pb.Response
+
+// denyRequest
+// ===========
+func (c *Chaincode) denyRequest(stub shim.ChaincodeStubInterface, args []string) pb.Response
 
 // Main
 // ====
