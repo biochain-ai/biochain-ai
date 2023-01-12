e33bc67109675027de93cda1db163a712bc737c1
Alessio Diana <alessio.diana@studenti.unipr.it>
2023-01-12 10:06:29 +0100

    Fixed.

--- eseguiEInstalla.sh
+++ eseguiEInstalla.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script per costruire la rete e installare il chiancode 'privatedata'
 
-echo "Inizio Script!!"
+echo "eseguiEInstalla start!!"
 
 echo " ---------------------------"
 echo " -- Starting the network -- "
@@ -13,18 +13,26 @@ echo "-- Copying code --"
 echo "------------------"
 cp -R ~/_uni/borsa/biochain-ai/privatedata/ ~/minifab/vars/chaincode/
 
+echo "---------------------------------------"
+echo "-- Copying collections configuration --"
+echo "---------------------------------------"
+cp ~/_uni/borsa/biochain-ai/privatedata_collection_config.json ~/minifab/vars/
+
 echo "--------------------------"
 echo "-- Installing chaincode --"
 echo "--------------------------"
-minifab install -n privatedata -r true
+#minifab install -n privatedata -r true
+minifab ccup -n privatedata -l go -v 1.0 -r true
 
-echo "------------------------------"
-echo "-- Replacing words with sed --"
-echo "------------------------------"
-sed -i 's/collectionPublic/collectionDatoBio/g' vars/privatedata_collection_config.json
-sed -i 's/collectionPrivate/collectionDatoBioPrivateDetails/g' vars/privatedata_collection_config.json
+# echo "------------------------------"
+# echo "-- Replacing words with sed --"
+# echo "------------------------------"
+# sed -i 's/collectionPublic/collectionDatoBio/g' vars/privatedata_collection_config.json
+# sed -i 's/collectionPrivate/collectionDatoBioPrivateDetails/g' vars/privatedata_collection_config.json
 
 echo "-------------------------------"
 echo "-- Approve,commit,initialize --"
 echo "-------------------------------"
 minifab approve,commit,initialize -p ''
+
+echo "eseguiEInstalla finish!!"
