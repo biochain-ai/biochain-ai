c7e18eff93cc308af6c22679ad013c60d4317c5c
Alessio Diana <alessio.diana@studenti.unipr.it>
2023-07-04 16:15:01 +0200

    Added ssl.

--- scripts/web-server.sh
+++ scripts/web-server.sh
@@ -5,7 +5,7 @@ function startWebServer() {
     # Needs the builded image to run.
     infoln "Starting Web Server"
     docker build --tag web-server-php  ./web-server-php/.
-    docker run -d --net biochain-ai_fabric-network --env GOOGLE_OAUTH_CLIENT_ID_TEST_CHAINCODE --env GOOGLE_OAUTH_CLIENT_SECRET_TEST_CHAINCODE -p 8080:80 --name web-server-php web-server-php
+    docker run -d --net biochain-ai_fabric-network --env GOOGLE_OAUTH_CLIENT_ID_TEST_CHAINCODE --env GOOGLE_OAUTH_CLIENT_SECRET_TEST_CHAINCODE -p 8080:80 -p 443:443 --name web-server-php web-server-php
 }
 
 # Kills and run a new web server
@@ -14,5 +14,5 @@ function restartWebServer (){
     docker rm web-server-php
     sleep 1s
     docker build --tag web-server-php  ./web-server-php/.
-    docker run -d --net biochain-ai_fabric-network --env GOOGLE_OAUTH_CLIENT_ID_TEST_CHAINCODE --env GOOGLE_OAUTH_CLIENT_SECRET_TEST_CHAINCODE -p 8080:80 --name web-server-php web-server-php
+    docker run -d --net biochain-ai_fabric-network --env GOOGLE_OAUTH_CLIENT_ID_TEST_CHAINCODE --env GOOGLE_OAUTH_CLIENT_SECRET_TEST_CHAINCODE -p 8080:80 -p 443:443 --name web-server-php web-server-php
 }
--- web-server-php/config.php
+++ web-server-php/config.php
@@ -6,7 +6,7 @@ $google_client = new Google_Client();
 // $google_client->setAuthConfigFile('client_secrets.json');
 $google_client->setClientId($_ENV['GOOGLE_OAUTH_CLIENT_ID_TEST_CHAINCODE']);
 $google_client->setClientSecret($_ENV['GOOGLE_OAUTH_CLIENT_SECRET_TEST_CHAINCODE']);
-$google_client->setRedirectUri('https://pc169.math.unipr.it:8080/oauth2callback.php');
+$google_client->setRedirectUri('https://pc169.math.unipr.it:443/oauth2callback.php');
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
